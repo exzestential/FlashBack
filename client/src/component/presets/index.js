c28e7065a536a8fb2f80bc3a4fad62dfd99b512d
exze-alegre <akina.alegre@gmail.com>
2025-04-25 21:36:00 +0800

    added index exporting

---
+++ client/src/component/presets/index.js
@@ -0,0 +1,5 @@
+export { default as Brand } from "./Brand";
+export { default as Header } from "./Header";
+export { default as Label } from "./Label";
+export { default as Paragraph } from "./Paragraph";
+export { default as Subheader } from "./Subheader";
